b823e64d688887b8684872b2a5fc919634896cf9
harish00506 <harish00506@outlook.com>
2025-11-30 21:22:31 +0530

    the basic set of the project

---
+++ src/main/java/com/tns/placementmanagment/PlacementmanagmentApplication.java
@@ -0,0 +1,13 @@
+package com.tns.placementmanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacementmanagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacementmanagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tns/placementmanagment/PlacementmanagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tns.placementmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacementmanagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
