149ccce3b2664e083d4040d3f82eabb6610e5d34
harish00506 <harish00506@outlook.com>
2025-12-02 11:17:46 +0530

    the final iteration

--- src/main/java/com/tns/placementmanagment/entities/Admin.java
+++ src/main/java/com/tns/placementmanagment/entities/Admin.java
@@ -1,6 +1,8 @@
 package com.tns.placementmanagment.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "admin")
